513f83a8007d6d7ac05adb701fb95c559429266e
Diogo Almeida <diogomean@gmail.com>
2017-07-09 02:15:52 -0300

    adição de funções de exclusão e alteração de filmes

--- routes/movies.js
+++ routes/movies.js
@@ -9,24 +9,42 @@ router.get('/', (req, res, next) => {
 
 router.get('/list-movies', (req, res) => {
   MovieModel.find((error, movies) => {
+    if (error) {res.send(error)}
     res.json({ status: 200, message: "Olá seja bem vindo a listagem de filmes", movies: movies })
   });
-
 });
 
 router.get('/find-movie/:id', (req, res) => {
-  MovieModel.findOne({ _id: req.params.id }, (erro, movie) => {
+  MovieModel.findOne({ _id: req.params.id }, (error, movie) => {
+    if (error) {res.send(error)}
     res.json(movie)
   })
 })
 
+router.put('/update-movie/:id', (req, res) => {
+  MovieModel.findOne({ _id: req.params.id }, (error, movie) => {
+    if (error) { res.send(error) }
+
+    movie.save().then((error, movie) => {
+      if (error) { res.send(error); }
+      res.json({ status: 200, message: "Filme alterado com sucesso", movie: movie });
+    })
+  })
+});
 
 router.post('/insert-movie', (req, res) => {
   let movie = new MovieModel(movieFake);
-  movie.save().then((err, movie) => {
+  movie.save().then((error, movie) => {
+    if (error) {res.send(error)}
     res.json({ status: 200, message: "Filme salvo com sucesso", movie: movie });
   })
+});
 
+router.delete('/remove-movie/:id', (req, res) => {
+  MovieModel.deleteOne({ _id: req.params.id }, (error, movie) => {
+    if (error) throw error;
+    res.json({ status: 200, message: "Filme excluido com sucesso" });
+  })
 });
 
 module.exports = router;
