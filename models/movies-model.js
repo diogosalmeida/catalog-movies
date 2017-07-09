aa8d30f882bbf29ce899d8fdabd81be873f18d2c
Diogo Almeida <diogomean@gmail.com>
2017-07-08 22:03:16 -0300

    Corrigindo criação de modelo

--- models/movies-model.js
+++ models/movies-model.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
-const connect = require('../configDB/connect');
+const connect = require('../configDB/connect')();
 const Schema = mongoose.Schema;
 
-const MovieSchema = new Schema({
+const MovieModel = new Schema({
     title: String,
     poster: String,
     direction: String,
     synopsis: String,
-    comments: [{ body: String, date: Date }],
-    release_year: string,
+    comments: [{ body: String, date: String }],
+    release_year: String,
     likes: Number,
     did_not_like: Number,
     budget: Number
 });
 
-module.exports = mongoose.model('MovieModel',MovieSchema);
+const Movie = mongoose.model('MovieModel',MovieModel);
+
+module.exports = Movie ;
