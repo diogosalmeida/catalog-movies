9f07cdb4fe4751f6d067ff236afd58a905b79e3e
Diogo Almeida <diogomean@gmail.com>
2017-07-08 22:03:56 -0300

    Criação de funções de salvar filme e listagem geral de filmes

--- routes/movies.js
+++ routes/movies.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const movie = require('../movie');
+const movieFake = require('../movie');
 const router = express.Router();
-
+const MovieModel = require('../models/movies-model');
 /* GET users listing. */
-router.get('/',(req, res, next) => {
+router.get('/', (req, res, next) => {
   res.send('Bem vindo a Api de filmes');
 });
 
-router.post('/insert-movie',(req,res)=>{
-  res.send('Cadastro de filme concluido com sucesso')
-})
+router.post('/insert-movie', (req, res) => {
+  let movie = new MovieModel(movieFake);
+  movie.save().then((err, movie) => {
+    res.json({ status: 200, message: "Filme salvo com sucesso", movie: movie });
+  })
 
-router.get('/list-movies',(req,res)=>{
-  res.json(movie);
-})
+});
+
+router.get('/list-movies', (req, res) => {
+  MovieModel.find((error, movies) => {
+    res.json({ status: 200, message: "Olá seja bem vindo a listagem de filmes", movies: movies })
+  });
+
+});
+
 module.exports = router;
