86137a86507df70f41cb989b62a9298259a213b1
Diogo Almeida <diogomean@gmail.com>
2017-07-09 02:51:47 -0300

    Adição de função de comentários

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const movieFake = require('../movie');
 const router = express.Router();
+const moment = require("moment");
 const MovieModel = require('../models/movies-model');
 /* GET users listing. */
 router.get('/', (req, res, next) => {
@@ -9,27 +10,27 @@ router.get('/', (req, res, next) => {
 
 router.get('/list-movies', (req, res) => {
   MovieModel.find((error, movies) => {
-    if (error) {res.send(error)}
+    if (error) { res.send(error) }
     res.json({ status: 200, message: "Olá seja bem vindo a listagem de filmes", movies: movies })
   });
 });
 
 router.get('/find-movie/:id', (req, res) => {
   MovieModel.findOne({ _id: req.params.id }, (error, movie) => {
-    if (error) {res.send(error)}
+    if (error) { res.send(error) }
     res.json(movie)
   })
 })
 
 
-router.put('/like-movie/:id',(req,res)=>{
-  MovieModel.findOne({_id:req.params.id},(error,movie)=>{
-     movie.likes += 1;
+router.put('/like-movie/:id', (req, res) => {
+  MovieModel.findOne({ _id: req.params.id }, (error, movie) => {
+    movie.likes += 1;
 
-      movie.save().then((error, movie) => {
+    movie.save().then((error, movie) => {
       if (error) { res.send(error); }
       res.json({ status: 200, message: "Sua curtida foi processada", movie: movie });
-    })    
+    })
   });
 });
 
@@ -44,10 +45,36 @@ router.put('/update-movie/:id', (req, res) => {
   })
 });
 
+router.put('/insert-comment/:id', (req, res) => {
+  MovieModel.findOne({ _id: req.params.id }, (error, movie) => {
+    if (error) { res.send(error) }
+
+    movie.comments.push(
+      {
+        body: "Uma bosta",
+        date: moment().format('DD-MM-YYYY')
+      },
+      {
+        body: "Um lixo",
+        date: moment().format('DD-MM-YYYY')
+      },
+      {
+        body: "Meu ovo",
+        date: moment().format('DD-MM-YYYY')
+      }
+    )
+
+    movie.save().then((error, movie) => {
+      if (error) { res.send(error); }
+      res.json({ status: 200, message: "Filme alterado com sucesso", movie: movie }).statusCode(200);
+    })
+  })
+});
+
 router.post('/insert-movie', (req, res) => {
   let movie = new MovieModel(movieFake);
   movie.save().then((error, movie) => {
-    if (error) {res.send(error)}
+    if (error) { res.send(error) }
     res.json({ status: 200, message: "Filme salvo com sucesso", movie: movie }).statusCode(200);
   })
 });
